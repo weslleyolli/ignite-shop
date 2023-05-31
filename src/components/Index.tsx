4f2c106e3150af7745333921bd614a42ad2e17a7
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-05-30 21:32:51 -0300

    New t-shirt add

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -19,12 +19,12 @@ export default function Index() {
     });
 
     const footerClasses = 'absolute bottom-1 left-1 right-1 p-8 rounded-md flex items-center justify-between bg-black bg-opacity-60 opacity-0 transition-all duration-200 hover:opacity-100'
-    const aClasses = 'keen-slider__slide bg-gradient-to-b from-teal-400 via-teal-500 to-indigo-500 rounded-lg p-1 cursor-pointer relative flex items-center justify-center object-cover'
+    const aClasses = 'keen-slider__slide bg-gradient-to-b from-teal-400 via-teal-500 to-indigo-500 rounded-lg cursor-pointer relative flex items-center justify-center object-cover'
 
     return (
         <div className="flex flex-col items-start justify-center min-h-screen gap-6" >
             <Header />
-            <main ref={sliderRef} className='keen-slider flex gap-12 w-full max-w-[calc(100vw-((100vw-1180px)/2))] ml-auto min-h-[656px] ' >
+            <main ref={sliderRef} className='keen-slider flex w-full max-w-[calc(100vw-((100vw-1180px)/2))] ml-auto min-h-[656px] ' >
                 <a href="" className={aClasses}>
                     <Image src={tshirt1} width={520} height={480} alt='' />
                     <footer className={footerClasses}>
